8849768f91c4047a0f313448d89a41d51dd7c1b3
aksghwoddl <aksghwoddl@naver.com>
2022-11-28 15:39:01 +0900

    1. Diary fragment 목표 , 소비 칼로리 추가
    2. 소비 칼로리에 따른 progressbar 추가
    
    3. 전체적인 레이아웃 및 string 수정 및 추가

--- FooDi/app/src/main/java/com/lee/foodi/ui/fragments/diary/DiaryFragment.kt
+++ FooDi/app/src/main/java/com/lee/foodi/ui/fragments/diary/DiaryFragment.kt
@@ -6,12 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.lee.foodi.R
 import com.lee.foodi.common.FoodiNewApplication
+import com.lee.foodi.common.Utils
+import com.lee.foodi.common.manager.FooDiPreferenceManager
 import com.lee.foodi.databinding.FragmentDiaryBinding
 import com.lee.foodi.ui.activities.search.SearchActivity
 
 class DiaryFragment : Fragment() {
     private lateinit var binding : FragmentDiaryBinding
+    private lateinit var mPreferenceManager: FooDiPreferenceManager
 
     companion object{
         fun newInstance() = DiaryFragment()
@@ -23,17 +27,45 @@ class DiaryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentDiaryBinding.inflate(inflater , container , false)
+        mPreferenceManager = FooDiPreferenceManager.getInstance(FoodiNewApplication.getInstance())
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        init()
         addListeners()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        updateCalories()
+    }
+
+    private fun init() {
+        updateCalories()
+        binding.spendCalorieProgressBar.progress = 70
+    }
+
+    private fun updateCalories() {
+        mPreferenceManager.goalCalorie?.let {
+            Utils.convertValueWithErrorCheck(binding.goalCalorieTextView
+                , getString(R.string.goal_calorie) ,
+                mPreferenceManager.goalCalorie!!
+            )
+        }
+        Utils.convertValueWithErrorCheck(binding.spendCalorieTextView , getString(R.string.spend_calorie) , "0")
     }
 
     private fun addListeners() {
         with(binding){
             searchButton.setOnClickListener {
+                with(Intent(FoodiNewApplication.getInstance() , SearchActivity::class.java)){
+                    startActivity(this)
+                }
+            }
+
+            addButton.setOnClickListener {
                 with(Intent(FoodiNewApplication.getInstance() , SearchActivity::class.java)){
                     startActivity(this)
                 }
