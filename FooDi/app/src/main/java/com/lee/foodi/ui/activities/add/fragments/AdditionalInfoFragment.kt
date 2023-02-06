c50e18034c1c4bde7413bfaefee5f6a4d6d064fc
aksghwoddl <aksghwoddl@naver.com>
2023-02-06 21:38:47 +0900

    Fragment BaseFragment class 만들어 사용하도록 수정
    - build.gradle 버전 업데이트
    - BaseFragment class 추가
    - 모든 Fragment class들 BaseFragment 상속받아 사용하도록 수정
    
    Todo.
    - 프로젝트에 hilt 추가하기

--- FooDi/app/src/main/java/com/lee/foodi/ui/activities/add/fragments/AdditionalInfoFragment.kt
+++ FooDi/app/src/main/java/com/lee/foodi/ui/activities/add/fragments/AdditionalInfoFragment.kt
@@ -4,18 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.databinding.library.baseAdapters.BR
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.lee.foodi.R
-import com.lee.foodi.data.repository.FoodiRepository
 import com.lee.foodi.databinding.FragmentAdditionalInfoBinding
 import com.lee.foodi.ui.activities.add.viewmodel.AddFoodViewModel
-import com.lee.foodi.ui.factory.FoodiViewModelFactory
+import com.lee.foodi.ui.fragments.BaseFragment
 
-class AdditionalInfoFragment : Fragment() {
-    private lateinit var binding : FragmentAdditionalInfoBinding
+class AdditionalInfoFragment : BaseFragment<FragmentAdditionalInfoBinding>(R.layout.fragment_additional_info) {
     private lateinit var mViewModel : AddFoodViewModel
     companion object{
         fun newInstance() = AdditionalInfoFragment()
@@ -26,10 +20,10 @@ class AdditionalInfoFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate(inflater , R.layout.fragment_additional_info , container , false)
+        val view = super.onCreateView(inflater, container, savedInstanceState)
         mViewModel = AddFoodViewModel.getInstance()!!
         binding.additionalViewModel = mViewModel
         binding.lifecycleOwner = this
-        return binding.root
+        return view
     }
 }
